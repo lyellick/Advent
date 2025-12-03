5ca853eaf1482d3d24caf299170fe89494beb2a1
Lincoln Yellick <lincoln.yellick@gmail.com>
2025-12-03 08:02:15 -0600

    Day 3 Part 1 Completed

---
+++ src/Advent.Solutions/Y2025/D03.cs
@@ -0,0 +1,49 @@
+﻿using Advent.Shared.Attributes;
+using Advent.Shared.Models;
+using System.Linq;
+
+namespace Advent.Solutions.Y2025
+{
+    [TestClass]
+    [Puzzle(2025, 3)]
+    public sealed class D03 : AdventTestBase
+    {
+        [TestMethod]
+        public void P01()
+        {
+            var batteries = Puzzle.Input.TrimEnd('\n').Split("\n").Select(pack => pack.Select(battery => int.Parse(battery.ToString())).ToArray());
+
+            List<int> joltages = [];
+
+            foreach (var battery in batteries) 
+            {
+                List<int> combos = [];
+                for (int i = 0; i < battery.Length; i++)
+                {
+                    for (int n = 0; n < battery.Length; n++)
+                    {
+                        if (n > i)
+                        {
+                            var joltage = int.Parse($"{battery[i]}{battery[n]}");
+                            combos.Add(joltage);
+                        }
+                    }
+                }
+                
+                var highest = combos.Max();
+
+                joltages.Add(highest);
+            }
+
+            var total = joltages.Sum();
+
+            Assert.AreEqual(17207, total);
+        }
+
+        [TestMethod]
+        public void P02()
+        {
+            Assert.Inconclusive("Part 2 not implemented.");
+        }
+    }
+}
