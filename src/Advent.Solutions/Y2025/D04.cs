f21ae923c625c9d89322a195aa032e1b4c6c9d67
Lincoln Hach Yellick <lincoln.yellick@kerberus.io>
2025-12-04 17:58:02 -0600

    Completed Day 4 Part 2

--- src/Advent.Solutions/Y2025/D04.cs
+++ src/Advent.Solutions/Y2025/D04.cs
@@ -59,79 +59,85 @@ namespace Advent.Solutions.Y2025
         [TestMethod]
         public void P02()
         {
-            var grid = Puzzle.Input.Split("\n").Select(row => row.Select(c => c == '.' ? 0 : 1).ToArray()).ToArray();
+            var grid = Puzzle.Input.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(row => row.TrimEnd('\r').Select(c => c == '.' ? 0 : 1).ToArray()).ToArray();
 
-            List<int> availableLocations = [];
+            List<int> rollsRemoved = [];
 
-            RemoveRolls(grid, availableLocations);
+            bool rollsWereRemoved = true;
 
-            int total = availableLocations.Sum();
-
-            Assert.Inconclusive("Part 2 not implemented.");
-        }
-
-        public string RemoveRolls(int[][] grid, List<int> availableLocations)
-        {
-            availableLocations = [];
-            bool changed = true;
-            string lastOutput = "";
-
-            while (changed)
+            while (rollsWereRemoved)
             {
-                List<int> available = [];
-                lastOutput = ProcessGrid(grid, available, out changed);
-                availableLocations.Add(available.Count);
-            }
+                rollsWereRemoved = false;
 
-            return lastOutput;
-        }
+                var rollsToRemove = new List<(int row, int col)>();
 
-        public string ProcessGrid(int[][] grid, List<int> removed, out bool changed)
-        {
-            changed = false;
-            return ProcessCell(grid, 0, 0, "", removed, ref changed);
-        }
-
-        public string ProcessCell(int[][] grid, int row, int col, string r, List<int> availableLocations, ref bool changed)
-        {
-            if (row >= grid.Length)
-                return r;
-
-            if (col >= grid[row].Length)
-                return ProcessCell(grid, row + 1, 0, r + "\n", availableLocations, ref changed);
-
-            int cell = grid[row][col];
-            string thing = cell == 1 ? "@" : ".";
-
-            if (cell == 1)
-            {
-                var topLeft = AdjacentCell(grid, row - 1, col - 1);
-                var top = AdjacentCell(grid, row, col - 1);
-                var topRight = AdjacentCell(grid, row + 1, col - 1);
-                var midLeft = AdjacentCell(grid, row - 1, col);
-                var midRight = AdjacentCell(grid, row + 1, col);
-                var botLeft = AdjacentCell(grid, row - 1, col + 1);
-                var bot = AdjacentCell(grid, row, col + 1);
-                var botRight = AdjacentCell(grid, row + 1, col + 1);
-
-                var totalAdjacent = topLeft + top + topRight + midLeft + midRight + botLeft + bot + botRight;
-
-                if (totalAdjacent < 4)
+                for (int row = 0; row < grid.Length; row++)
                 {
-                    availableLocations.Add(1);
-                    thing = "X";
-                    grid[row][col] = 0;
-                    changed = true;
+                    int len = grid[row].Length;
+
+                    for (int col = 0; col < len; col++)
+                    {
+                        if (grid[row][col] == 0)
+                        {
+                            continue;
+                        }
+
+                        int adjacent = 0;
+
+                        for (int adjacentRow = row - 1; adjacentRow <= row + 1; adjacentRow++)
+                        {
+                            for (int adjacentCol = col - 1; adjacentCol <= col + 1; adjacentCol++)
+                            {
+                                if (adjacentRow == row && adjacentCol == col)
+                                {
+                                    continue;
+                                }
+
+                                if (adjacentRow < 0 || adjacentRow >= grid.Length)
+                                {
+                                    continue;
+                                }
+
+                                if (adjacentCol < 0 || adjacentCol >= grid[adjacentRow].Length)
+                                {
+                                    continue;
+                                }
+
+                                adjacent += grid[adjacentRow][adjacentCol];
+                            }
+                        }
+
+                        if (adjacent < 4)
+                        {
+                            rollsToRemove.Add((row, col));
+                        }
+                    }
                 }
+
+                if (rollsToRemove.Count == 0)
+                {
+                    rollsRemoved.Add(0);
+                    break;
+                }
+
+                foreach (var (row, col) in rollsToRemove)
+                {
+                    grid[row][col] = 0;
+                }
+
+                rollsRemoved.Add(rollsToRemove.Count);
+
+                rollsWereRemoved = true;
             }
 
-            return ProcessCell(grid, row, col + 1, r + thing, availableLocations, ref changed);
+            int total = rollsRemoved.Sum();
+
+            Assert.AreEqual(8758, total);
         }
 
         public int AdjacentCell(int[][] grid, int row, int col)
         {
-            if (row >= 0 && row < grid.Length &&
-                col >= 0 && col < grid[row].Length)
+            if (row >= 0 && row < grid.Length && col >= 0 && col < grid[row].Length)
             {
                 return grid[row][col];
             }
