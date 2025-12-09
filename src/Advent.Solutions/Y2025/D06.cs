86ea96ae193f1281883fc2090ce4a28e724e0d78
Lincoln Hach Yellick <lincoln.yellick@kerberus.io>
2025-12-08 18:17:55 -0600

    Completed Day 6 Part 1 Working through part 2

---
+++ src/Advent.Solutions/Y2025/D06.cs
@@ -0,0 +1,79 @@
+﻿using Advent.Shared.Attributes;
+using Advent.Shared.Models;
+
+namespace Advent.Solutions.Y2025
+{
+    [TestClass]
+    [Puzzle(2025, 6)]
+    public sealed class D06 : AdventTestBase
+    {
+        [TestMethod]
+        public void P01()
+        {
+            var rows = Puzzle.Input.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(row => row.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray()).ToArray();
+
+            long total = 0;
+
+            for (int i = 0; i < rows[0].Length; i++)
+            {
+                var a = long.Parse(rows[0][i]);
+                var b = long.Parse(rows[1][i]);
+                var c = long.Parse(rows[2][i]);
+                var d = long.Parse(rows[3][i]);
+
+                switch (rows[4][i])
+                {
+                    case "*":
+                        total += a * b * c * d;
+                        break;
+                    case "+":
+                        total += a + b + c + d;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(8108520669952, total);
+        }
+
+        [TestMethod]
+        public void P02()
+        {
+            var rows = Puzzle.Input.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(row => row.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray()).ToArray();
+
+            long total = 0;
+
+            for (int i = 0; i < rows[0].Length; i++)
+            {
+                var a = rows[0][i];
+                var b = rows[1][i];
+                var c = rows[2][i];
+                var d = rows[3][i];
+
+                var problems = new[] { a, b, c, d };
+
+                var width = problems.Max(w => w.Length);
+
+                var cols = new List<char[]>();
+
+                foreach (var problem in problems)
+                {
+                    var parts = problem.PadLeft(width).ToCharArray();
+
+                    cols.Add(parts);
+                }
+
+                switch (rows[4][i])
+                {
+                    case "*":
+                        //total += a * b * c * d;
+                        break;
+                    case "+":
+                        //total += a + b + c + d;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(8108520669952, total);
+        }
+    }
+}
